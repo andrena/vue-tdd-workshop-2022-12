783f83dce284f750b8e85bb336a69b26da5eb13e
Michael Zugelder <michael.zugelder@andrena.de>
2022-12-08 13:19:42 +0100

    Lösung

--- frontend-vue/test/cucumber/features/Customer.feature
+++ frontend-vue/test/cucumber/features/Customer.feature
@@ -2,6 +2,16 @@ Feature: Manage customers
 
   Scenario: Creating and activating a customer
     Given the starting page is loaded
+    Given the current user is "foo+#testid#@bar.de"
 
-#    When ...
-#    Then ...
+    When the current user fills out the form and submits it
+    Then the current user appears inactive in the table
+
+    When the current user clicks on "Send activation"
+    Then the current user appears pending in the table
+
+    When the current user clicks on the activation link in the e-mail
+    Then the confirmation page shows that the current user is active
+
+    Given the starting page is loaded
+    Then the current user appears active in the table
--- frontend-vue/test/cucumber/step_definitions/customer.steps.ts
+++ frontend-vue/test/cucumber/step_definitions/customer.steps.ts
@@ -1,6 +1,80 @@
-import { Given } from "@cucumber/cucumber";
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import type { CustomerStatus, CustomerUnsaved } from "commons";
 import { CustomerPage } from "../../pages/CustomerPage";
+import { getActivationLinkFromMailhog } from "../../base/getActivationLinkFromMailhog";
+import { ActivationPage } from "../../pages/ActivationPage";
+
+const newCustomer: CustomerUnsaved = {
+  firstName: "Test",
+  lastName: "Test",
+  emailAddress: "test@test.de",
+  phoneNumber: "+49151231545",
+  addresses: [
+    {
+      city: "Berlin",
+      zipCode: "10000",
+      streetAndNumber: "Strasse 1",
+      country: "Germany",
+    },
+  ],
+};
 
 Given(/^the starting page is loaded$/, async function () {
   await this.open(CustomerPage);
 });
+
+Given(/^the current user is "([^"]+)"$/, function (user: string) {
+  this.data.currentUser = this.replace(user);
+});
+
+When(/^the current user fills out the form and submits it$/, async function () {
+  const page = this.get(CustomerPage);
+  await page.addCustomer({
+    ...newCustomer,
+    emailAddress: this.data.currentUser,
+  });
+});
+
+When(
+  /^the current user searches for their email in the table$/,
+  async function () {
+    const page = this.get(CustomerPage);
+    await page.filter(this.data.currentUser);
+  }
+);
+
+Then(
+  /^the current user appears (inactive|pending|active) in the table$/,
+  async function (status: CustomerStatus) {
+    const page = this.get(CustomerPage);
+    expect(await page.getStatusForCustomer(this.data.currentUser)).toBe(status);
+  }
+);
+
+When(/^the current user clicks on "Send activation"$/, async function () {
+  const page = this.get(CustomerPage);
+  await page.sendActivation(this.data.currentUser);
+});
+
+When(
+  /^the current user clicks on the activation link in the e-mail$/,
+  async function () {
+    const activationLink = await getActivationLinkFromMailhog(
+      this.data.currentUser
+    );
+    expect(activationLink).toBeDefined();
+    await this.attach("Activation link: " + activationLink);
+
+    await this.open(ActivationPage, activationLink);
+  }
+);
+
+Then(
+  /^the confirmation page shows that the current user is active$/,
+  async function () {
+    const page = this.get(ActivationPage);
+    const text = await page.getActivationMessage(this.data.currentUser);
+    expect(text).toContain("active");
+  }
+);
