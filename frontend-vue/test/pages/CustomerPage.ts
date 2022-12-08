87fc8761b8ee9baa2ac88bf42a284b8640d9a30f
Michael Zugelder <michael.zugelder@andrena.de>
2022-12-08 13:19:39 +0100

    Übung 6

--- frontend-vue/test/pages/CustomerPage.ts
+++ frontend-vue/test/pages/CustomerPage.ts
@@ -1,4 +1,5 @@
-import { expect, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
+import type { CustomerUnsaved } from "commons";
 import { BasePage } from "./BasePage";
 
 export class CustomerPage extends BasePage {
@@ -15,4 +16,33 @@ export class CustomerPage extends BasePage {
     await expect(this.getPageHeader()).toHaveText("Customer List");
   }
 
+  get table(): Locator {
+    return this.page.getByRole("table");
+  }
+
+  async addCustomer(customer: CustomerUnsaved): Promise<void> {
+    const form = this.page.locator("form", { hasText: "Add a Customer" });
+
+    await form.getByText("First name").fill(customer.firstName);
+    await form.getByText("Last name").fill(customer.lastName);
+    await form.getByText("Email address").fill(customer.emailAddress);
+    await form.getByText("Phone number").fill(customer.phoneNumber);
+
+    const [address, ...otherAdresses] = customer.addresses;
+    if (!address || otherAdresses.length > 0) {
+      throw new Error("Multiple or zero addresses are not supported yet");
+    }
+    await form.getByText("Street and number").fill(address.streetAndNumber);
+    await form.getByText("Zip code").fill(address.zipCode);
+    await form.getByText("City").fill(address.city);
+    await form.getByText("Country").fill(address.country);
+
+    await form.getByText("Add customer").click();
+  }
+
+  async filter(filter: string): Promise<void> {
+    const searchbox = this.page.getByRole("searchbox");
+    await searchbox.fill(filter);
+    await searchbox.press("Enter");
+  }
 }
--- frontend-vue/test/ui/CustomerPage.spec.ts
+++ frontend-vue/test/ui/CustomerPage.spec.ts
@@ -1,9 +1,65 @@
+import { expect } from "@playwright/test";
 import { test } from "./test";
+import { createTestCustomer } from "commons/dist/helpers/createTestCustomer";
+import { CustomerPage } from "../pages/CustomerPage";
 
-test("should open and render the table", async () => {
-  // ...
+const john = createTestCustomer({
+  id: "1",
+  firstName: "John",
+  lastName: "Doe",
+  phoneNumber: "+491000000000",
+  emailAddress: "john.doe@mail.de",
+  addresses: [
+    {
+      city: "Berlin",
+      zipCode: "10000",
+      streetAndNumber: "Teststrasse 1",
+      country: "Deutschland",
+    },
+  ],
 });
 
-test("should display a newly added customer", async () => {
-  // ...
+const ben = createTestCustomer({
+  id: "2",
+  firstName: "Benjamin",
+  lastName: "Utzer",
+  phoneNumber: "+491111110011",
+  emailAddress: "ben.utzer@mail.de",
+  addresses: [
+    {
+      city: "Berlin",
+      zipCode: "10000",
+      streetAndNumber: "Teststrasse 2",
+      country: "Deutschland",
+    },
+  ],
+});
+
+test("should open and render the table", async ({ page, backendMock }) => {
+  backendMock.intercept({
+    method: "GET",
+    path: "/api/",
+    reply: { body: [john] },
+  });
+
+  const customerPage = await CustomerPage.open(page);
+  await expect(customerPage.table).toContainText("John");
+  await expect(customerPage.table).toContainText("Doe");
+});
+
+test("should display a newly added customer", async ({ page, backendMock }) => {
+  backendMock
+    .intercept({ method: "GET", path: "/api/", reply: { body: [john] } })
+    .intercept({
+      method: "POST",
+      path: "/api/",
+      reply: { body: ben },
+    });
+
+  const customerPage = await CustomerPage.open(page);
+  await expect(customerPage.table).toContainText("John");
+
+  await customerPage.addCustomer(ben);
+  await expect(customerPage.table).toContainText("Benjamin");
+  await expect(customerPage.table).toContainText("Utzer");
 });
